5bfd99f182b8cb353dc67f797ca51d2ecae4e609
Vedant <vedant0711@gmail.com>
2024-06-10 18:14:07 +0530

    Authentication done

--- podcastr/app/(root)/page.tsx
+++ podcastr/app/(root)/page.tsx
@@ -1,13 +1,21 @@
+"use client";
 import React from 'react'
 import { podcastData } from '@/constants';
 import PodcastCard from '@/components/ui/PodcastCard';
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+
+
+
+
 
 const page = () => {
+  
   return (
     <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
       <section className='flex flex-col gap-5'>
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
-
         <div className="podcast_grid">
           {podcastData.map(({ id, title, description, imgURL }) => (
             <PodcastCard 
---
+++ podcastr/convex/auth.config.ts
@@ -0,0 +1,11 @@
+ const authConfig={
+    providers: [
+      {
+        domain: "https://present-titmouse-15.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+
+  export default authConfig;
